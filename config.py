e95c739f5034e76ef2f174d1456f373f8c5aca8e
juaninho <molini1405@gmail.com>
2026-07-23 14:48:41 -0300

    Corrijo configuracion SMTP Gmail

--- config.py
+++ config.py
@@ -28,9 +28,9 @@ class Config:
 
     # ===== EMAIL =====
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
 
     # PONÉ TU GMAIL
     MAIL_USERNAME = 'mini.tentaciones.ok@gmail.com'
