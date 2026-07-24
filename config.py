b3546bf3123ea9f2e1fcff5572ecc4956148122d
juaninho <molini1405@gmail.com>
2026-07-23 21:18:29 -0300

    Limpio sistema de correo y dejo Resend

--- config.py
+++ config.py
@@ -26,13 +26,3 @@ class Config:
         'static',
         'uploads'
     )
-
-    # ===== EMAIL =====
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
